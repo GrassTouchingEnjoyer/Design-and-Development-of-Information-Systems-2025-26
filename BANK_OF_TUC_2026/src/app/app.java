47960c1a0b7352483666d67e795fa59006e77304
grass <147980342+GrassTouchingEnjoyer@users.noreply.github.com>
2025-12-25 21:16:02 +0200

    Add files via upload

---
+++ BANK_OF_TUC_2026/src/app/app.java
@@ -0,0 +1,17 @@
+package app;
+
+import static utils.utils.print;
+
+public class app{
+
+	public class main {
+	
+	    public static void main(String[] args){
+	
+	        print("Hello, World!");
+
+	        System.exit(0);
+	    }
+	}
+
+}
