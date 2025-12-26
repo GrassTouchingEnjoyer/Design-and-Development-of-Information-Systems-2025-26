456d272a6aa6986e3b65d73370720b91b97e60fa
Konstantinos Adamakopoulos <110929671+kadamakopoulos@users.noreply.github.com>
2025-12-26 22:16:01 +0200

    Implement toString method in Manager class
    Added toString method to Manager class for better string representation.

--- BANK_OF_TUC_2026/src/back_end/actors/Manager.java
+++ BANK_OF_TUC_2026/src/back_end/actors/Manager.java
@@ -5,4 +5,15 @@ public class Manager extends Administrator {
     public Manager(String username, String password, String employeeId, String role, String managementLevel) {
         super(username, password, employeeId, role, managementLevel);
     }
+
+    @Override
+    public String toString() {
+    return "Manager{" +
+           "username='" + getUsername() + '\'' +
+           ", employeeId='" + getEmployeeId() + '\'' +
+           ", role='" + getRole() + '\'' +
+           ", managementLevel='" + getManagementLevel() + '\'' +
+           '}';
+    }
+
 }
