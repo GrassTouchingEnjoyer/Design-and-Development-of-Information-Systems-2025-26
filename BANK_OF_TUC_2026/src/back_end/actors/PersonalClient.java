c3d8d12a8133f6b07f8e6879bb84512240d5bb60
Konstantinos Adamakopoulos <110929671+kadamakopoulos@users.noreply.github.com>
2025-12-26 22:13:02 +0200

    Update PersonalClient.java

--- BANK_OF_TUC_2026/src/back_end/actors/PersonalClient.java
+++ BANK_OF_TUC_2026/src/back_end/actors/PersonalClient.java
@@ -5,4 +5,15 @@ public class PersonalClient extends Client {
     public PersonalClient(String username, String password, String fullName, String afm, String phone) {
         super(username, password, fullName, afm, phone);
     }
+
+    @Override
+    public String toString() {
+        return "PersonalClient{" +
+               "username='" + getUsername() + '\'' +
+               ", fullName='" + getFullName() + '\'' +
+               ", afm='" + getAfm() + '\'' +
+               ", phone='" + getPhone() + '\'' +
+               '}';
+    }
+
 }
